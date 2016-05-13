483461cc5dd5e078e5aaba1e0b3fd1dfa16ffe32
Alistair Mackay <alistair.mackay.work@gmail.com>
2016-05-13 11:54:23 +0100

    still not working

--- console.rb
+++ console.rb
@@ -2,7 +2,7 @@ require_relative( '../models/pizza' ) 
 require_relative( '../models/sales' )
 require( 'pry-byebug' )
 
-pizzas = Pizza.all(  )
+pizzas = Pizza.all()
 sales = Sales.new( pizzas )
 
 binding.pry
--- models/pizza.rb
+++ models/pizza.rb
@@ -1,21 +1,20 @@
- require( 'pry-byebug' ) # break point debugger.
- require( 'pg' )# gem for PreSQL
+ require( 'pry-byebug' )
+ require( 'pg' )
 
  class Pizza
 
    attr_accessor( :first_name, :last_name, :topping, :quantity )
 
-   def initialize( options ) #options hash
+   def initialize( options )
      @id = options[ 'id' ].to_i
-     @first_name = options[ 'first_name' ] #techincally this is a hash
+     @first_name = options[ 'first_name' ]
      @last_name = options[ 'last_name' ]
      @topping = options[ 'topping' ]
      @quantity = options[ 'quantity' ].to_i
    end
 
-#save method, instance method.
-   def save(  ) #connects to database file
-     db = PG.connect( { dbname: 'pizza_shop', host: 'localhost' } ) #tells ruby where your database is located using a hash.
+   def save(  )
+     db = PG.connect( { dbname: 'pizza_shop', host: 'localhost' } )
      sql = "INSERT INTO pizzas (
       first_name,
       last_name,
@@ -27,18 +26,17 @@
       '#{ @topping }',
       #{@quantity}
       );"
-      #above links are ruby instance variables with PreSQL code.
 
-      db.exec( sql ) #executes takes your sql and runs it on your database.
-      db.close() #shuts off your db connection(full stop!)
+      db.exec( sql )
+      db.close()
    end
 
-   def self.all() #defining our own class method. #self key-word represents what you're currently in.
+   def self.all()
      db = PG.connect( { dbname: 'pizza_shop', host: 'localhost' } )
      sql = "SELECT * FROM pizzas"
      pizzas = db.exec( sql )
      db.close(  )
-     return pizzas.map { |pizza| Pizza.new( pizza ) } #feeds each row back into our pizza class to convert the values to the required
+     return pizzas.map { |pizza| Pizza.new( pizza ) }
    end
 
    def update()
@@ -53,7 +51,7 @@
      db.close
    end
 
-   def delete() #instance method - as it's the users responsibility to
+   def delete()
      db = PG.connect( { dbname: 'pizza_shop', host: 'localhost' } )
      sql = "DELETE FROM pizzas WHERE id = #{@id}"
      db.exec( sql )
